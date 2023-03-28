e989a47c71c29d5cc60cf7064986fbc89fb1c7ea
Malavika Harikumar <malavikahari02@gmail.com>
2023-03-28 11:54:47 +0530

    implemented signatures to protect private key

--- ecdsa-node/client/src/Wallet.jsx
+++ ecdsa-node/client/src/Wallet.jsx
@@ -1,31 +1,51 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
+function Wallet({
+    address,
+    setAddress,
+    balance,
+    setBalance,
+    privateKey,
+    setPrivateKey,
+}) {
+    async function onChange(evt) {
+        // generate public key and address from private key
+        const _privateKey = evt.target.value;
+        setPrivateKey(_privateKey);
+
+        const publicKey = secp.getPublicKey(_privateKey);
+        const publicKeyHash = toHex(keccak256(publicKey));
+        // console.log("Public key Hash wallet", publicKeyHash);
+        const address = `0x${publicKeyHash.slice(-20)}`; // 20 bytes address
+        setAddress(address);
+        if (address) {
+            const {
+                data: { balance },
+            } = await server.get(`balance/${address}`);
+            setBalance(balance);
+        } else {
+            setBalance(0);
+        }
     }
-  }
 
-  return (
-    <div className="container wallet">
-      <h1>Your Wallet</h1>
-
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
-
-      <div className="balance">Balance: {balance}</div>
-    </div>
-  );
+    return (
+        <div className="container wallet">
+            <h1>Your Wallet</h1>
+            <label>
+                Private Key
+                <input
+                    placeholder="Type in a Private Key"
+                    value={privateKey}
+                    onChange={onChange}
+                ></input>
+            </label>
+            <div>Address: {address}</div>{" "}
+            <div className="balance">Balance: {balance}</div>
+        </div>
+    );
 }
 
 export default Wallet;
---
+++ ecdsa-node/client/src/helper.js
@@ -0,0 +1,17 @@
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils.js";
+
+export const generateSignature = async (amount, recipient, privateKey) => {
+    const uint8Array = Uint8Array.from([amount, recipient]);
+    const messageHash = toHex(uint8Array);
+    // console.log("Message Hash = ", messageHash);
+    const [signature, recovery] = await secp.sign(messageHash, privateKey, {
+        recovered: true,
+    });
+
+    const signatureHex = toHex(signature);
+
+    const recoveryBit = recovery;
+
+    return { signatureHex, recoveryBit };
+};
